cfa7e832ec920e0e4c8d543ab4c9c847e5a80ca2
seangel2003 <skg99@scarletmail.rutgers.edu>
2025-09-27 19:53:16 -0400

    From prototype to first playable

---
+++ Assets/_Scripts/MissionDemolition.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode {                                                           
+    idle, 
+    playing,
+    levelEnd
+}
+
+public class MissionDemolition : MonoBehaviour
+{
+    static private MissionDemolition S;
+
+    [Header("Inscribed")]
+    public Text uitLevel;  
+    public Text uitShots;  
+    public Vector3 castlePos; 
+    public GameObject[] castles;
+
+    [Header("Dynamic")]
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;   
+    public GameMode mode = GameMode.idle;
+    public string showing = "Show Slingshot";
+
+    void Start() {
+        S = this; 
+
+        level = 0;
+        shotsTaken = 0;
+        levelMax = castles.Length;
+        StartLevel();
+    }
+
+    void StartLevel() {
+        // Get rid of the old castle if one exists
+        if (castle != null) {
+            Destroy( castle );
+        }
+
+        // Destroy old projectiles if they exist (the method is not yet written)
+        Projectile.DESTROY_PROJECTILES(); 
+
+        // Instantiate the new castle
+        castle = Instantiate<GameObject>( castles[level] );
+        castle.transform.position = castlePos;
+
+        // Reset the goal
+        Goal.goalMet = false;
+
+        UpdateGUI();
+
+        mode = GameMode.playing;
+
+        // Zoom out to show both
+        FollowCam.SWITCH_VIEW( FollowCam.eView.both );
+    }
+
+    void UpdateGUI() {
+        // Show the data in the GUITexts
+        uitLevel.text = "Level: "+(level+1)+" of "+levelMax;
+        uitShots.text = "Shots Taken: "+shotsTaken;
+    }
+
+    void Update() {
+        UpdateGUI();
+
+        // Check for level end
+        if ( (mode == GameMode.playing) && Goal.goalMet ) {
+            // Change mode to stop checking for level end
+            mode = GameMode.levelEnd;
+            // Zoom out to show both
+            FollowCam.SWITCH_VIEW( FollowCam.eView.both );
+            // Start the next level in 2 seconds
+            Invoke("NextLevel", 2f);                                          
+        }
+    }
+
+    void NextLevel() {                                                         
+        level++;
+        if (level == levelMax) {
+            level = 0;
+            shotsTaken = 0;
+        }
+        StartLevel();
+    }
+
+    // Static method that allows code anywhere to increment shotsTaken
+    static public void SHOT_FIRED() {                                          
+        S.shotsTaken++;
+    }
+
+    // Static method that allows code anywhere to get a reference to S.castle
+    static public GameObject GET_CASTLE() {                                   
+        return S.castle;
+    }
+}
